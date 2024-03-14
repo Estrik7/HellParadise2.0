6fab0836448783201501104cb527e779d3787e89
Estrik7 <stiven_libre@hotmail.com>
2024-03-14 07:11:05 -0500

    marge entre version de Brandon y version de Leando y Hodi

--- HellParadise/views.py
+++ HellParadise/views.py
@@ -1,4 +1,6 @@
 #region imports
+from tabulate import tabulate
+from django.contrib.auth.decorators import login_required
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from io import BytesIO
 from django.core.files import File
@@ -7,27 +9,35 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from HellParadise.models import Categoria, Producto
-#endregion
 
+#endregion
 def index(request):
-    return render(request, 'index.html')
+    pertenece_al_grupo = request.user.groups.filter(name='usuario_operador').exists()
+    context = {'pertenece_al_grupo': pertenece_al_grupo}
+
+    if pertenece_al_grupo:
+        return render(request, 'index.html', context)
+    else:
+        mensaje_error = "No tienes permisos de operador para acceder a esta función."
+        context['mensaje_error'] = mensaje_error
+        return render(request, 'index.html', context)
 
 #region usuario
 def registro_usuario(request):
-    if request.method=='POST':
-        user=authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
+    if request.method == 'POST':
+        user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
         if request.POST.get('nombre') and request.POST.get('apellido') and request.POST.get('username') and request.POST.get('email') and request.POST.get('password'):
-            usuario = User.objects.create_user(request.POST.get('username'),request.POST.get('email'),request.POST.get('password'))
+            usuario = User.objects.create_user(request.POST.get('username'), request.POST.get('email'), request.POST.get('password'))
             usuario.first_name = request.POST.get('nombre')
             usuario.last_name = request.POST.get('apellido')
             usuario.save()
-            return redirect('login')
+            return redirect('/login')
     else:
         return render(request, 'usuario/registro_usuario.html')
 
 def loginusuario(request):
     if request.method == "POST":
-        usuario = request.POST.get('usuario')
+        usuario = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=usuario, password=password)
         if user is not None:
@@ -47,160 +57,236 @@ def logoutusuario(request):
 #endregion
 
 #region categoria
+@login_required
 def insertar_categoria(request):
-    if request.method == "POST":
-        if request.POST.get('nombre'):
-            categoria = Categoria()
-            categoria.nombre = request.POST.get('nombre')
-            categoria.save()
-            return redirect('insertar_categoria')
-    else:
-        categoria = Categoria.objects.all()
-        return render(request, 'producto/insertar_categoria.html', {'categoria': categoria})
+    pertenece_al_grupo = request.user.groups.filter(name='usuario_operador').exists()
+    context = {'pertenece_al_grupo': pertenece_al_grupo}
 
-def actualizar_categoria(request, id_categoria):
-        if request.method == 'POST':
-                categoria = Categoria.objects.get(id=id_categoria)
+    if pertenece_al_grupo:
+        if request.method == "POST":
+            if request.POST.get('nombre'):
+                categoria = Categoria()
                 categoria.nombre = request.POST.get('nombre')
                 categoria.save()
-                return redirect('lista_categoria')
+                return redirect('/insertar_categoria/')
+        else:
+            categoria = Categoria.objects.all()
+            context['categoria'] = categoria
+    else:
+        context['mensaje_error'] = "No tienes permisos de operador para acceder a esta función."
+
+    return render(request, 'producto/insertar_categoria.html', context)
+
+@login_required
+def actualizar_categoria(request, id_categoria):
+    pertenece_al_grupo = request.user.groups.filter(name='usuario_operador').exists()
+    context = {'pertenece_al_grupo': pertenece_al_grupo}
+
+    if pertenece_al_grupo:
+        if request.method == 'POST':
+            categoria = Categoria.objects.get(id=id_categoria)
+            categoria.nombre = request.POST.get('nombre')
+            categoria.save()
+            return redirect('/producto/listado_categorias')
         else:
             un_categoria = Categoria.objects.filter(id=id_categoria)
-            return render(request, 'producto/actualizar_categoria.html', {'un_categoria': un_categoria})
+            context['un_categoria'] = un_categoria
+    else:
+        context['mensaje_error'] = "No tienes permisos de operador para acceder a esta función."
 
+    return render(request, 'producto/actualizar_categoria.html', context)
+
+@login_required
 def listado_categoria(request):
-    categorias = Categoria.objects.all
-    return render(request, "producto/listado_categoria.html", {"categorias": categorias})
+    pertenece_al_grupo = request.user.groups.filter(name='usuario_operador').exists()
+    context = {'pertenece_al_grupo': pertenece_al_grupo}
 
-def eliminar_categoria(request,id_categoria):
+    if pertenece_al_grupo:
+        categorias = Categoria.objects.all()
+        context['categorias'] = categorias
+    else:
+        context['mensaje_error'] = "No tienes permisos de operador para acceder a esta función."
+
+    return render(request, "producto/listado_categoria.html", context)
+
+@login_required
+def eliminar_categoria(request, id_categoria):
+    pertenece_al_grupo = request.user.groups.filter(name='usuario_operador').exists()
+    context = {'pertenece_al_grupo': pertenece_al_grupo}
+
+    if pertenece_al_grupo:
         eliminar_categoria = Categoria.objects.get(id=id_categoria)
         eliminar_categoria.delete()
-        return redirect('lista_categoria')
+        return redirect('/producto/listado_categorias')
+    else:
+        context['mensaje_error'] = "No tienes permisos de operador para acceder a esta función."
+
+    return render(request, 'index.html', context)
+
 #endregion
 
 #region producto
+@login_required
 def insertar_producto(request):
-    if request.method == "POST" and request.FILES:
-        if (
-            request.POST.get('nombre') and
-            request.POST.get('descripcion') and
-            request.POST.get('precio') and
-            request.POST.get('stock') and
-            request.POST.get('categoria_id') and
-            request.POST.get('talla') and
-            request.POST.get('color')
-        ):
-            # Recopila todos los datos del formulario
-            nombre = request.POST.get('nombre')
-            descripcion = request.POST.get('descripcion')
-            precio = request.POST.get('precio')
-            stock = request.POST.get('stock')
-            categoria_id = request.POST.get('categoria_id')
-            talla = request.POST.get('talla')
-            color = request.POST.get('color')
+    pertenece_al_grupo = request.user.groups.filter(name='usuario_operador').exists()
+    context = {'pertenece_al_grupo': pertenece_al_grupo}
 
-            # Crea una instancia de Producto
-            producto = Producto(
-                nombre=nombre,
-                descripcion=descripcion,
-                precio=precio,
-                stock=stock,
-                categoria_id=categoria_id,
-                talla=talla,
-                color=color
-            )
+    if pertenece_al_grupo:
+        if request.method == "POST" and request.FILES:
+            if (
+                request.POST.get('nombre') and
+                request.POST.get('descripcion') and
+                request.POST.get('precio') and
+                request.POST.get('stock') and
+                request.POST.get('categoria_id') and
+                request.POST.get('talla') and
+                request.POST.get('color')
+            ):
+                # Recopila todos los datos del formulario
+                nombre = request.POST.get('nombre')
+                descripcion = request.POST.get('descripcion')
+                precio = request.POST.get('precio')
+                stock = request.POST.get('stock')
+                categoria_id = request.POST.get('categoria_id')
+                talla = request.POST.get('talla')
+                color = request.POST.get('color')
+
+                # Crea una instancia de Producto
+                producto = Producto(
+                    nombre=nombre,
+                    descripcion=descripcion,
+                    precio=precio,
+                    stock=stock,
+                    categoria_id=categoria_id,
+                    talla=talla,
+                    color=color
+                )
+
+                # Procesa la imagen con Pillow si es necesario
+                imagen = request.FILES['imagenes']
+                imagen_pillow = Image.open(imagen)
+                
+                # Convertir la imagen a modo RGB si está en modo RGBA
+                if imagen_pillow.mode == 'RGBA':
+                    imagen_pillow = imagen_pillow.convert('RGB')
+                
+                imagen_pillow = imagen_pillow.resize((1200, 1200))
+
+                # Crea un objeto BytesIO y guarda la imagen en él
+                imagen_temp_io = BytesIO()
+                imagen_pillow.save(imagen_temp_io, format='JPEG')
+
+                # Crea un objeto File a partir del BytesIO
+                imagen_temp = File(imagen_temp_io, name=imagen.name)
+
+                # Asigna la imagen al campo 'imagenes' del producto
+                producto.imagenes.save(imagen.name, imagen_temp, save=True)
+
+                # Guarda el producto en la base de datos
+                producto.save()
+
+                return redirect('/insertar_producto/')
+        else:
+            categorias = Categoria.objects.all()
+            context['categoria'] = categorias
+    else:
+        context['mensaje_error'] = "No tienes permisos de operador para acceder a esta función."
+
+    return render(request, 'producto/insertar_producto.html', context)
+
+@login_required
+def listado_producto(request):
+    pertenece_al_grupo = request.user.groups.filter(name='usuario_operador').exists()
+    context = {'pertenece_al_grupo': pertenece_al_grupo}
+
+    if pertenece_al_grupo:
+        productos = Producto.objects.all()
+
+        # Formatear el precio para tener separadores de miles con puntos
+        for producto in productos:
+            try:
+                # Intentar convertir el precio a float
+                precio_float = float(producto.precio)
+                # Formatear el precio como entero
+                producto.precio = '{:,.0f}'.format(precio_float)
+            except (ValueError, TypeError):
+                # Manejar casos en los que el precio no es un número válido
+                producto.precio = 'N/A'
+
+        context['productos'] = productos
+        return render(request, "producto/listado_producto.html", context)
+    else:
+        mensaje_error = "No tienes permisos de operador para acceder a esta función."
+        context['mensaje_error'] = mensaje_error
+        return render(request, 'index.html', context)
+
+
+@login_required
+def actualizar_producto(request, id_producto):
+    pertenece_al_grupo = request.user.groups.filter(name='usuario_operador').exists()
+    context = {'pertenece_al_grupo': pertenece_al_grupo}
+
+    if pertenece_al_grupo:
+        producto = Producto.objects.get(id=id_producto)
+
+        if request.method == "POST" and request.FILES:
+            # Recopila todos los datos del formulario
+            producto.nombre = request.POST.get('nombre')
+            producto.descripcion = request.POST.get('descripcion')
+            producto.precio = request.POST.get('precio')
+            producto.stock = request.POST.get('stock')
+            producto.categoria_id = request.POST.get('categoria_id')
+            producto.talla = request.POST.get('talla')
+            producto.color = request.POST.get('color')
 
             # Procesa la imagen con Pillow si es necesario
             imagen = request.FILES['imagenes']
             imagen_pillow = Image.open(imagen)
-            
+
             # Convertir la imagen a modo RGB si está en modo RGBA
             if imagen_pillow.mode == 'RGBA':
                 imagen_pillow = imagen_pillow.convert('RGB')
-            
+
             imagen_pillow = imagen_pillow.resize((1200, 1200))
 
             # Crea un objeto BytesIO y guarda la imagen en él
             imagen_temp_io = BytesIO()
             imagen_pillow.save(imagen_temp_io, format='JPEG')
 
-            # Crea un objeto File a partir del BytesIO
-            imagen_temp = File(imagen_temp_io, name=imagen.name)
+            # Crea un objeto InMemoryUploadedFile a partir del BytesIO
+            imagen_temp = InMemoryUploadedFile(
+                imagen_temp_io, None, imagen.name, 'image/jpeg', imagen_temp_io.tell(), None
+            )
 
             # Asigna la imagen al campo 'imagenes' del producto
-            producto.imagenes.save(imagen.name, imagen_temp, save=True)
+            producto.imagenes = imagen_temp
 
-            # Guarda el producto en la base de datos
+            # Guarda el producto actualizado en la base de datos
             producto.save()
 
-            return redirect('insertar_producto')
-    else:
+            return redirect('/producto/listado_producto/')  # Reemplaza con tu URL de listado de productos
+
         categorias = Categoria.objects.all()
-        return render(request, 'producto/insertar_producto.html', {'categoria': categorias}) 
+        context['categoria'] = categorias
+        context['producto'] = producto
+    else:
+        context['mensaje_error'] = "No tienes permisos de operador para acceder a esta función."
 
-def listado_producto(request):
-    productos = Producto.objects.all()
-
-    # Formatear el precio para tener separadores de miles con puntos
-    for producto in productos:
-        try:
-            # Intentar convertir el precio a float
-            precio_float = float(producto.precio)
-            # Formatear el precio como entero
-            producto.precio = '{:,.0f}'.format(precio_float)
-        except (ValueError, TypeError):
-            # Manejar casos en los que el precio no es un número válido
-            producto.precio = 'N/A'
-
-    return render(request, "producto/listado_producto.html", {'productos': productos})
-
-def actualizar_producto(request, id_producto):
-    producto = Producto.objects.get(id=id_producto)
-
-    if request.method == "POST" and request.FILES:
-        # Recopila todos los datos del formulario
-        producto.nombre = request.POST.get('nombre')
-        producto.descripcion = request.POST.get('descripcion')
-        producto.precio = request.POST.get('precio')
-        producto.stock = request.POST.get('stock')
-        producto.categoria_id = request.POST.get('categoria_id')
-        producto.talla = request.POST.get('talla')
-        producto.color = request.POST.get('color')
-
-        # Procesa la imagen con Pillow si es necesario
-        imagen = request.FILES['imagenes']
-        imagen_pillow = Image.open(imagen)
-
-        # Convertir la imagen a modo RGB si está en modo RGBA
-        if imagen_pillow.mode == 'RGBA':
-            imagen_pillow = imagen_pillow.convert('RGB')
-
-        imagen_pillow = imagen_pillow.resize((1200, 1200))
-
-        # Crea un objeto BytesIO y guarda la imagen en él
-        imagen_temp_io = BytesIO()
-        imagen_pillow.save(imagen_temp_io, format='JPEG')
-
-        # Crea un objeto InMemoryUploadedFile a partir del BytesIO
-        imagen_temp = InMemoryUploadedFile(
-            imagen_temp_io, None, imagen.name, 'image/jpeg', imagen_temp_io.tell(), None
-        )
-
-        # Asigna la imagen al campo 'imagenes' del producto
-        producto.imagenes = imagen_temp
-
-        # Guarda el producto actualizado en la base de datos
-        producto.save()
-
-        return redirect('producto/lista_producto')  # Reemplaza con tu URL de listado de productos
-
-    categorias = Categoria.objects.all()
-    return render(request, 'producto/actualizar_producto.html', {'categoria': categorias, 'producto': producto})
+    return render(request, 'producto/actualizar_producto.html', context)
 
 
-def eliminar_producto(request,id_producto):
-    eliminar_producto = Producto.objects.get(id=id_producto)
-    eliminar_producto.delete()
-    return redirect('lista_producto')
+@login_required
+def eliminar_producto(request, id_producto):
+    pertenece_al_grupo = request.user.groups.filter(name='usuario_operador').exists()
+    context = {'pertenece_al_grupo': pertenece_al_grupo}
+
+    if pertenece_al_grupo:
+        eliminar_producto = Producto.objects.get(id=id_producto)
+        eliminar_producto.delete()
+        return redirect('/producto/listado_producto')
+    else:
+        context['mensaje_error'] = "No tienes permisos de operador para acceder a esta función."
+
+    return render(request, 'index.html', context)
 
 #endregion
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # HellParadise2.0
 Ecomers de ropa con Django
+
++ Manual técnico
+    Requisitos
+  - Visual Studio Code (o algun editor de código)
+  - Python 3.11.2
+    - Pillow 10.1.0
+    - mysqlclient 2.2.0  
+  - Django 4.2.3
+  - Servidor local como Xampp o Wampserver e instalar el servidor de apache 3.3.0 y MySQL
+  - MySQL
+
+paso a paso
+despues de tener todos los archivos, abrir cualquiera de los servidores locales con el Apache 3.3.0 activado y el MySQL importar la base de datos para luego introducir este comando
+$ py manage.py runserver
